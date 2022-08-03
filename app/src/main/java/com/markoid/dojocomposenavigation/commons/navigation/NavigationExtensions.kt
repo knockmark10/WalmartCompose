2a157189f43014a4bf364fa4dcfaa6508d9fd3fa
Marco Chavez <vn52mu1>
2022-08-03 17:13:46 -0500

    Extending navigation extension

--- app/src/main/java/com/markoid/dojocomposenavigation/commons/navigation/NavigationExtensions.kt
+++ app/src/main/java/com/markoid/dojocomposenavigation/commons/navigation/NavigationExtensions.kt
@@ -1,14 +1,26 @@
 package com.markoid.dojocomposenavigation.commons.navigation
 
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import androidx.navigation.NavOptionsBuilder
 import com.markoid.dojocomposenavigation.commons.navigation.NavScreens.Cart
 import com.markoid.dojocomposenavigation.commons.navigation.NavScreens.Home
 import com.markoid.dojocomposenavigation.commons.navigation.NavScreens.PreHome
 import com.markoid.dojocomposenavigation.commons.navigation.NavScreens.Splash
 
-fun NavController.navigate(navScreen: NavScreens) = when (navScreen) {
-  PreHome, Splash -> navigate(route = navScreen.name)
-  is Home -> navigate(route = "${navScreen.name}/${navScreen.brand}")
-  is Cart -> navigate(route = "${navScreen.name}?cartId=${navScreen.cartId},isAnonymous=${navScreen.isAnonymous}")
+fun NavController.navigate(
+  navScreen: NavScreens,
+  builder: NavOptionsBuilder.() -> Unit = {}
+) = when (navScreen) {
+  PreHome, Splash -> navigate(
+    route = navScreen.name,
+    builder = builder
+  )
+  is Home -> navigate(
+    route = "${navScreen.name}/${navScreen.brand}",
+    builder = builder
+  )
+  is Cart -> navigate(
+    route = "${navScreen.name}?cartId=${navScreen.cartId},isAnonymous=${navScreen.isAnonymous}",
+    builder = builder
+  )
 }
